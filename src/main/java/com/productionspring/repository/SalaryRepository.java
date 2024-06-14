cb24d848a9cc5f3ce7c37861668d4f806eb3a056
Bekastalk <azamatovbekmamat518@gmail.com>
2024-06-14 17:34:17 +0600

    add repository and dependensies

---
+++ src/main/java/com/productionspring/repository/SalaryRepository.java
@@ -0,0 +1,13 @@
+package com.productionspring.repository;
+
+import kg.kstu.production.entity.Salary;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface SalaryRepository extends JpaRepository<Salary, Long> {
+    public List<Salary> findAllByYearAndMonth(Integer year, Integer month);
+    public Boolean findFirstByYearAndMonth(Integer year, Integer month);
+}
